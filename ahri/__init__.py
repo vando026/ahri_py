08f25559357d9b22b8b8480f252d4ae5a7e0878b
Alain <albundy79@gmail.com>
2022-02-06 14:34:09 +0100

    Add env.yml file

--- ahri/__init__.py
+++ ahri/__init__.py
@@ -4,7 +4,11 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import os
+import statsmodels.api as sm
+import patsy
+import multiprocessing as mp
 
-from . import  dataproc
+from . import dataproc
 from . import args
 from . import utils
+from . import calc
