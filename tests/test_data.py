c6307e74d8bcbbdea4a1ed2b462a0300eed353ae
Alain <albundy79@gmail.com>
2022-02-08 22:28:19 +0100

    Make tests self contained

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,71 +3,71 @@ import sys
 from ahri import utils
 from ahri.args import SetFiles, SetArgs
 from ahri.dataproc import DataProc
-from ahri.api import API
 import numpy as np
+import os
 
 # in ipython run
 # %run test_data.py "/home/alain/Seafile/AHRI_Data/2020"
 
 class TestAHRI(unittest.TestCase):
 
-    root = '/home/alain/Seafile/AHRI_Data/2020'
-    args = SetArgs(root = root,
-        years = np.arange(2005, 2020))
-    testy = DataProc(args)
-    hdat = testy.get_hiv()
-    sdat = testy.set_hiv()
-    rtdat = testy.get_repeat_testers(sdat)
-    edat = testy.get_epi()
-    esdat = testy.set_epi()
+    root = os.path.dirname(os.path.realpath(__file__)) 
+    root = '/home/alain/Seafile/Programs/Python/library/ahri_dev/tests'
+    args = SetArgs(root = root, years = np.arange(2005, 2020),
+            drop_tasp = False)
+    args.hiv_pkl = os.path.join(root, 'python/hiv.pkl')
+    args.epi_pkl = os.path.join(root, 'python/epi.pkl')
+    dtest = DataProc(args)
+
+    hdat = dtest.get_hiv()
+    sdat = dtest.set_hiv()
+    rtdat = dtest.get_repeat_testers(sdat)
+    edat = dtest.get_epi()
+    esdat = dtest.set_epi()
 
     def test_hiv_nrows(self):
         nrows = self.hdat.shape[0]
-        self.assertEqual(nrows, 185243)
+        self.assertEqual(nrows, 500)
 
     def test_hivpos_n(self):
         hiv_vals = self.hdat.HIVPositive.count()
-        self.assertEqual(hiv_vals, 47861)
+        self.assertEqual(hiv_vals, 150)
 
     def test_fem_n(self):
         fem_n = self.hdat.Female[self.hdat.Female == 1].count()
-        self.assertEqual(fem_n, 125836)
+        self.assertEqual(fem_n, 344)
 
     def test_hiv_nrows2(self):
         nrows = self.sdat.shape[0]
-        self.assertEqual(nrows, 115141)
+        self.assertEqual(nrows, 305)
 
     def test_hiv_fem_n2(self):
         fem_n = self.sdat.Female[self.sdat.Female == 1].count()
-        self.assertEqual(fem_n, 72892)
+        self.assertEqual(fem_n, 192)
 
     def test_hiv_year(self):
         year = np.mean(self.sdat.Year).round(3)
-        self.assertEqual(year, 2011.937)
-        year = np.mean(self.sdat.Year).round(3)
-        self.assertEqual(year, 2011.937)
+        self.assertEqual(year, 2011.157)
 
     def test_rt_nrow(self):
         nrow = self.rtdat.shape[0]
-        self.assertEqual(nrow, 20994)
+        self.assertEqual(nrow, 50)
         nval = self.rtdat.sero_event.sum()
-        self.assertEqual(nval, 3176)
+        self.assertEqual(nval, 8)
 
     def test_epi_nrow(self):
         nrow = self.edat.shape[0]
-        self.assertEqual(nrow, 6399800)
+        self.assertEqual(nrow, 500)
         nrow = self.edat.Female.value_counts()
-        self.assertEqual(nrow[0], 3000277)
+        self.assertEqual(nrow[0], 197)
 
     def test_sepi_nrow(self):
         nrow = self.esdat.shape[0]
-        self.assertEqual(nrow, 2716305)
+        self.assertEqual(nrow, 179)
         nrow = self.esdat.Female.value_counts()
-        self.assertEqual(nrow[0], 1321503)
+        self.assertEqual(nrow[0], 93)
         nsum = self.esdat.Age.sum()
-        self.assertEqual(nsum, 76810970)
+        self.assertEqual(nsum, 7037)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        TestAHRI.root = sys.argv.pop()
     unittest.main()
