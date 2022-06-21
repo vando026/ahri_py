7c8f207b69737b92bd8c6e5d97e9971d3731b5e6
Alain <albundy79@gmail.com>
2022-06-21 14:13:08 -0700

    Updates to SetFiles and SetArgs

--- src/ahri/args.py
+++ src/ahri/args.py
@@ -28,13 +28,21 @@ class SetFiles:
         name of men's .pkl general health dataset
     bst_pkl : str
         name of bounded structures .pkl dataset
-    pip_pkl : str
-        name of pip .pkl dataset
 
     Methods
     -------
-    set_path()
-        set the root path to the ahri .dta datasets
+    set_path : set the root path to the ahri .dta datasets
+    path_hiv_dta : str : change path name of hiv .dta dataset
+    path_epi_dta : str : change path name of surviellance .dta dataset
+    path_wgh_dta : str : change path name of women's .dta general health dataset
+    path_mgh_dta : str : change path name of men's .dta general health dataset
+    path_bst_dta : str : change path name of bounded structurs .dta dataset
+    path_hiv_pkl : str : change path name of hiv .pkl dataset
+    path_epi_pkl : str : change path name of surveillance .pkl dataset
+    path_wgh_pkl : str : change path name of women's .pkl general health dataset
+    path_mgh_pkl : str : change path name of men's .pkl general health dataset
+    path_bst_pkl : str : change path name of bounded structurs .pkl dataset
+    path_pip_pkl : str : change path name of pip .pkl dataset
     """
 
     def setpath(self, x): 
@@ -46,20 +54,13 @@ class SetFiles:
 
         return os.path.join(self.root, x)
 
-    def __init__(self, root, 
-        hiv_dta = "RD05-99 ACDIS HIV All.dta",
-        epi_dta = "SurveillanceEpisodes.dta",
-        wgh_dta = "RD03-99 ACDIS WGH ALL.dta",
-        mgh_dta = "RD04-99 ACDIS MGH ALL.dta", 
-        bst_dta = "RD01-03 ACDIS BoundedStructures.dta",
-        hiv_pkl = "ACDIS_HIV_All.pkl", 
-        epi_pkl = "SurveillanceEpisodes.pkl",
-        wgh_pkl = "ACDIS_WGH_ALL.pkl", 
-        mgh_pkl = "ACDIS_MGH_ALL.pkl", 
-        bst_pkl = "ACDIS_BoundedStructures.pkl",
-        pip_pkl = "ACDIS_PIP.pkl"):
+    def __init__(self, root):
         """
         Paramaters
+        ----------
+        root : the name of the root path to the AHRI datasets
+
+        Attributes
         ----------
         root : the name of the root path to the AHRI datasets
         hiv_dta : name of hiv .dta dataset
@@ -74,19 +75,56 @@ class SetFiles:
         bst_pkl : name of bounded structurs .pkl dataset
         pip_pkl : name of pip .pkl dataset
         """
-
         self.root = root
-        self.hiv_dta = self.setpath(hiv_dta)
-        self.epi_dta = self.setpath(epi_dta)
-        self.wgh_dta = self.setpath(wgh_dta)
-        self.mgh_dta = self.setpath(mgh_dta)
-        self.bst_dta = self.setpath(bst_dta)
-        self.hiv_pkl = self.setpath(hiv_pkl)
-        self.epi_pkl = self.setpath(epi_pkl)
-        self.wgh_pkl = self.setpath(wgh_pkl)
-        self.mgh_pkl = self.setpath(mgh_pkl)
-        self.bst_pkl = self.setpath(bst_pkl)
-        self.pip_pkl = self.setpath(pip_pkl)
+        self.hiv_dta = self.setpath("RD05-99 ACDIS HIV All.dta")
+        self.epi_dta = self.setpath("SurveillanceEpisodes.dta")
+        self.wgh_dta = self.setpath("RD03-99 ACDIS WGH ALL.dta")
+        self.mgh_dta = self.setpath("RD04-99 ACDIS MGH ALL.dta")
+        self.bst_dta = self.setpath("RD01-03 ACDIS BoundedStructures.dta")
+        self.hiv_pkl = self.setpath("ACDIS_HIV_All.pkl") 
+        self.epi_pkl = self.setpath("SurveillanceEpisodes.pkl")
+        self.wgh_pkl = self.setpath("ACDIS_WGH_ALL.pkl") 
+        self.mgh_pkl = self.setpath("ACDIS_MGH_ALL.pkl") 
+        self.bst_pkl = self.setpath("ACDIS_BoundedStructures.pkl")
+
+    def path_hiv_dta(self, file = "RD05-99 ACDIS HIV All.dta"):
+        self.hiv_dta = self.setpath(file)
+        return self.hiv_dta
+
+    def path_epi_dta(self, file = "SurveillanceEpisodes.dta"):
+        self.epi_dta = self.setpath(file)
+        return self.epi_dta
+
+    def path_wgh_dta(self, file = "RD03-99 ACDIS WGH ALL.dta"):
+        self.wgh_dta = self.setpath(file)
+        return self.wgh_dta
+
+    def path_mgh_dta(self, file = "RD04-99 ACDIS MGH ALL.dta"):
+        self.mgh_dta = self.setpath(file)
+        return self.mgh_dta
+
+    def path_bst_dta(self, file = "RD01-03 ACDIS BoundedStructures.dta"):
+        self.bst_dta = self.setpath(file)
+        return self.bst_dta
+
+    def path_hiv_pkl(self, file = "ACDIS_HIV_All.pkl"):
+        self.hiv_pkl = self.setpath(file)
+        return self.hiv_pkl
+
+    def path_epi_pkl(self, file = "SurveillanceEpisodes.pkl"):
+        return self.setpath(file)
+
+    def path_wgh_pkl(self, file = "ACDIS_WGH_ALL.pkl"):
+        self.epi_pkl = self.setpath(file)
+        return self.epi_pkl
+
+    def path_mgh_pkl(self, file = "ACDIS_MGH_ALL.pkl"):
+        self.mgh_pkl = self.setpath(file)
+        return self.mgh_pkl
+
+    def path_bst_pkl(self, file = "ACDIS_BoundedStructures.pkl"):
+        self.bst_pkl = self.setpath(file)
+        return self.mgh_pkl
 
     def show_dta(self): 
         """
@@ -105,7 +143,7 @@ class SetFiles:
                 self.mgh_pkl, self.wgh_pkl, sep = "\n")
 
 
-class SetArgs:
+class SetArgs(SetFiles):
     """
     A class for setting and storing user supplied parameters
 
@@ -134,31 +172,9 @@ class SetArgs:
         uses the multiprocessing library to select cores
     verbose : bool 
         show the progress bar
-    hiv_dta : str
-        name of hiv .dta dataset
-    epi_dta : str
-        name of surviellance .dta dataset
-    wgh_dta : str
-        name of women's .dta general health dataset
-    mgh_dta : str
-        name of men's .dta general health dataset
-    bst_dta : str
-        name of bounded structurs .dta dataset
-    hiv_pkl : str
-        name of hiv .pkl dataset
-    epi_pkl : str
-        name of surviellance .pkl dataset
-    wgh_pkl : str
-        name of women's .pkl general health dataset
-    mgh_pkl : str
-        name of men's .pkl general health dataset
-    bst_pkl : str
-        name of bounded structurs .pkl dataset
-    pip_pkl : str
-        name of pip .pkl dataset
     """
 
-    def __init__(self, paths, 
+    def __init__(self, root, 
         years = np.arange(2005, 2020),
         age = {"Fem": [15, 49], "Mal": [15, 54]}, 
         ageby = 5,
@@ -189,11 +205,11 @@ class SetArgs:
             uses the multiprocessing library to select cores
 
         """
-
+        super().__init__(root)
         self.agecat = np.arange(np.min(list(age.values())),
           np.max(list(age.values())) + ageby, ageby)
-        sdict = {"Fem": 1, "Mal": 0}
-        self.sex = {s:sdict[s] for s in age.keys()}
+        self.sdict = {"Fem": 1, "Mal": 0}
+        self.sex = {s:self.sdict[s] for s in age.keys()}
         self.years = years
         self.age = age
         self.ageby = ageby
@@ -202,14 +218,35 @@ class SetArgs:
         self.mcores = mcores
         self.verbose = verbose
         self.drop_tasp = drop_tasp
-        self.hiv_dta = paths.hiv_dta
-        self.epi_dta = paths.epi_dta        
-        self.wgh_dta = paths.wgh_dta
-        self.mgh_dta = paths.mgh_dta
-        self.bst_dta = paths.bst_dta
-        self.hiv_pkl = paths.hiv_pkl
-        self.epi_pkl = paths.epi_pkl
-        self.wgh_pkl = paths.wgh_pkl
-        self.mgh_pkl = paths.mgh_pkl
-        self.bst_pkl = paths.bst_pkl
-        self.pip_pkl = paths.pip_pkl
+
+    def update_years(self, years):
+        self.years = years
+        return self.years
+
+    def update_age(self, age):
+        """
+        Update the age ranges for males ("Mal") and females ("Fem")
+
+        Parameters
+        ----------
+        age : dict 
+        """
+        self.age = age
+        self.sex = {s:self.sdict[s] for s in age.keys()}
+        return self.age
+
+    def update_ageby(self, ageby):
+        self.ageby = ageby
+        return self.ageby
+
+    def update_impute_method(self, method):
+        self.imp_method = method
+        return self.imp_method
+
+    def update_drop_tasp(self, drop):
+        self.drop_tasp = drop
+        return self.drop_tasp
+
+    def update_nsim(self, nsim):
+        self.nsim = nsim
+        return self.nsim
--- tests/test_data.py
+++ tests/test_data.py
@@ -16,15 +16,13 @@ class TestAHRI(unittest.TestCase):
 
     # root = os.path.dirname(os.path.realpath(__file__)) 
     root = '/home/alain/Seafile/Programs/Python/library/ahri_dev/src/ahri/data'
-    fpaths = SetFiles(root, 
-        hiv_dta = "RD05-99_ACDIS_HIV_Sample.dta",
-        bst_dta = "RD01-03_ACDIS_BS_Sample.dta",
-        epi_dta = "SurveillanceEpisodes_Sample.dta",
-        wgh_dta = "RD03-99_ACDIS_WGH_Sample.dta",
-        mgh_dta = "RD04-99_ACDIS_MGH_Sample.dta")
-
-    args = SetArgs(paths = fpaths, verbose = False)
-    dtest = DataProc(args)
+    targs = SetArgs(root = root, verbose = False)
+    targs.path_hiv_dta("RD05-99_ACDIS_HIV_Sample.dta")
+    targs.path_bst_dta("RD01-03_ACDIS_BS_Sample.dta")
+    targs.path_epi_dta("SurveillanceEpisodes_Sample.dta")
+    targs.path_wgh_dta("RD03-99_ACDIS_WGH_Sample.dta")
+    targs.path_mgh_dta("RD04-99_ACDIS_MGH_Sample.dta")
+    dtest = DataProc(targs)
 
     bdat = dtest.bst_dta(write_pkl = False)
     hdat = dtest.hiv_dta(write_pkl = False)
@@ -76,10 +74,9 @@ class TestAHRI(unittest.TestCase):
 
 
     def test_get_hiv1(self):
-        args = SetArgs(paths = self.fpaths,
-            years = np.arange(2007, 2015), 
-            age = {"Fem": [40, 80]})
-        dtest = DataProc(args)
+        self.targs.update_years(np.arange(2007, 2015))
+        self.targs.update_age({"Fem": [40, 80]})
+        dtest = DataProc(self.targs)
         hdat = dtest.get_hiv()
         hiv1 = dtest.set_data(hdat)
         self.assertEqual(np.unique(hiv1.Female).tolist(), [1]) 
@@ -88,11 +85,10 @@ class TestAHRI(unittest.TestCase):
         self.assertEqual(hiv1.IIntID.tolist(), [740])
 
     def test_get_hiv0(self):
-        args = SetArgs(paths = self.fpaths,
-            years = np.arange(2005, 2011), 
-            age = {"Mal": [20, 25]},
-            drop_tasp = False)
-        dtest = DataProc(args)
+        self.targs.update_years(np.arange(2005, 2011))
+        self.targs.update_age({"Mal": [20, 25]})
+        self.targs.update_drop_tasp(drop = False)
+        dtest = DataProc(self.targs)
         hdat = dtest.get_hiv()
         hiv = dtest.set_data(hdat)
         hiv = dtest.calc_age_cat(hiv)
@@ -103,10 +99,9 @@ class TestAHRI(unittest.TestCase):
         self.assertEqual(len(hiv.loc[hiv.AgeCat.astype(str) == "[20, 25)", "IIntID"]), 1)
 
     def test_age_cat(self):
-        args = SetArgs(paths = self.fpaths,
-          years = np.arange(2001, 2018),
-          age = {"Fem": [15, 54], "Mal": [15, 54]})
-        dtest = DataProc(args)
+        self.targs.update_years(np.arange(2001, 2018))
+        self.targs.update_age({"Fem": [15, 54], "Mal": [15, 54]})
+        dtest = DataProc(self.targs)
         hdat = dtest.get_hiv()
         hiv = dtest.set_data(hdat)
         hiv = dtest.calc_age_cat(hiv)
@@ -122,10 +117,9 @@ class TestAHRI(unittest.TestCase):
         self.assertEqual(len(hiv.loc[hiv.AgeCat.astype(str) == "[70, 75)", "IIntID"]), 0)
     
     def test_hiv_equal(self):
-        args = SetArgs(paths = self.fpaths,
-          years = np.arange(2005, 2017),
-          age = {"Fem": [15, 54], "Mal": [15, 54]})
-        dtest = DataProc(args)
+        self.targs.update_years(np.arange(2005, 2017))
+        self.targs.update_age({"Fem": [15, 54], "Mal": [15, 54]})
+        dtest = DataProc(self.targs)
         hdat = dtest.get_hiv()
         hiv = dtest.set_data(hdat)
         hiv1 = dtest.set_hiv()
@@ -135,11 +129,9 @@ class TestAHRI(unittest.TestCase):
         self.assertTrue(all(hiv1 == hiv))
 
     def test_epi_equal(self):
-        args = SetArgs(paths = self.fpaths,
-            years = np.arange(2010, 2020), 
-            age = {"Fem": [16, 60], "Mal": [16, 60]},
-            verbose = False)
-        dtest = DataProc(args)
+        self.targs.update_years(np.arange(2010, 2020))
+        self.targs.update_age({"Fem": [16, 60], "Mal": [16, 60]})
+        dtest = DataProc(self.targs)
         edat = dtest.epi_dta(write_pkl = False)
         edat = dtest.set_data(edat)
         edat1 = dtest.set_epi()
